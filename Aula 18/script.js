296b7358cd7ac9239f29d515e9e3db6265a3caf0
Lívia Bastos <liviabastoslirio@gmail.com>
2025-05-13 20:43:13 -0300

    Aula 18

---
+++ Aula 18/script.js
@@ -0,0 +1,5 @@
+document.write("<h3>Olá, Mundo!</h3>"); 
+//* window.alert("Olá, Mundo!"); */
+/*  document.getElementById("resposta").innerHTML = window.
+confirm("você está gostando do curso"); 'p'*/
+ document.getElementById("resposta").innerHTML = "O meu nome é" + window.prompt("Qual é o seu nome?");
---
+++ Aula 18/script2.js
@@ -0,0 +1,4 @@
+function enviaDados()
+{
+   document.getElementById("inputusuario").innerHTML = "Nome enviado foi:" + window.prompt ("Digite seu nome:");
+}
